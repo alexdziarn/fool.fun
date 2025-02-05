30adcec631eacc89f52fd20f73d1a9687d5a4892
alex-windows-pc <alexdziarn@gmail.com>
2025-02-05 16:51:22 -0500

    add schema for graphql server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,8 +4,52 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 
 // Define your GraphQL schema
 const typeDefs = `#graphql
+  type Coin {
+    id: ID!
+    name: String!
+    ticker: String!
+    description: String!
+    picture: String!
+    currentPrice: Float!
+    owner: String!
+    incrementMultiple: Float!
+  }
+
+  type Account {
+    id: ID!
+    wallet: String!
+    coinsCreated: [Coin!]!
+    replies: [Reply!]!
+    coinsOwned: [Coin!]!
+  }
+
+  type Reply {
+    id: ID!
+    account: Account!
+    coin: Coin!
+    comment: String!
+  }
+
   type Query {
-    hello: String
+    getCoin(id: ID!): Coin
+    getAccount(id: ID!): Account
+    getReply(id: ID!): Reply
+  }
+
+  type Mutation {
+    createCoin(
+      name: String!
+      ticker: String!
+      description: String!
+      picture: String!
+      currentPrice: Float!
+      owner: String!
+      incrementMultiple: Float!
+    ): Coin
+
+    createAccount(wallet: String!): Account
+
+    createReply(accountId: ID!, coinId: ID!, comment: String!): Reply
   }
 `;
 
