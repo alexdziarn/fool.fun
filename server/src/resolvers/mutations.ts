283cf7c5d1f2f87000cf370c6542a5207748c528
alex-windows-pc <alexdziarn@gmail.com>
2025-02-07 16:28:25 -0500

    update mutations

--- server/src/resolvers/mutations.ts
+++ server/src/resolvers/mutations.ts
@@ -3,9 +3,13 @@ import { database } from "../firebase";
 
 export const Mutation = {
   createCoin: async (
-    _,
-    { name, ticker, description, picture, currentPrice, owner, incrementMultiple }
-  ) => {
+    name: string,
+    ticker: string,
+    description: string,
+    picture: string,
+    currentPrice: number,
+    owner: string,
+    incrementMultiple: number ) => {
     const newCoinRef = push(ref(database, "coins"));
     const newCoin = {
       name,
@@ -19,16 +23,24 @@ export const Mutation = {
     await set(newCoinRef, newCoin);
     return { id: newCoinRef.key, ...newCoin };
   },
-  createAccount: async (_, { wallet }) => {
+  createAccount: async ( wallet: string ) => {
     const newAccountRef = push(ref(database, "accounts"));
-    const newAccount = { wallet, coinsCreated: [], replies: [], coinsOwned: [] };
+    const userName: string = wallet.substring(0, 8);
+    const newAccount = { userName, wallet, coinsCreated: [], replies: [], coinsOwned: [] };
     await set(newAccountRef, newAccount);
     return { id: newAccountRef.key, ...newAccount };
   },
-  createReply: async (_, { accountId, coinId, comment }) => {
+  createReply: async ( accountId: string, coinId: string, comment: string, time: string ) => {
+    // time var will be converted to time type in frontend
     const newReplyRef = push(ref(database, "replies"));
-    const newReply = { accountId, coinId, comment };
+    const newReply = { accountId, coinId, comment, time };
     await set(newReplyRef, newReply);
     return { id: newReplyRef.key, ...newReply };
   },
+  createTransaction: async ( coinId: string, type: string, from: string, to: string, time: string ) => {
+    const newTransactionRef = push(ref(database, "transactions"));
+    const newTransaction = { coin: coinId, type, from, to, time };
+    await set(newTransactionRef, newTransaction);
+    return { id: newTransactionRef.key, ...newTransaction };
+  }
 };
